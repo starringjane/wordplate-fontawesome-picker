10266edafc0e175ab270a043aa616e5b9e419a42
Maxim Vanhove <maxim@starringjane.com>
2022-03-02 20:27:39 +0100

    Add support for wordplate/acf 12

--- src/Fields/IconPicker.php
+++ src/Fields/IconPicker.php
@@ -2,32 +2,18 @@
 
 namespace StarringJane\WordplateFontawesomePicker\Fields;
 
-use WordPlate\Acf\Fields\Field;
-use WordPlate\Acf\Fields\Attributes\Pending;
-use WordPlate\Acf\Fields\Attributes\Disabled;
-use WordPlate\Acf\Fields\Attributes\Required;
-use WordPlate\Acf\Fields\Attributes\Placeholder;
-use WordPlate\Acf\Fields\Attributes\DefaultValue;
-use WordPlate\Acf\Fields\Attributes\Instructions;
-use WordPlate\Acf\Fields\Attributes\ConditionalLogic;
-
-class IconPicker extends Field
+class IconPicker extends \WordPlate\Acf\Fields\Text
 {
-    use ConditionalLogic;
-    use DefaultValue;
-    use Disabled;
-    use Instructions;
-    use Pending;
-    use Placeholder;
-    use Required;
-
-    protected $type = 'text';
-
     public function __construct(string $label, ?string $name = null)
     {
         parent::__construct($label, $name);
 
-        $this->config->set('readonly', true);
-        $this->config->set('wrapper', ['class' => 'fontawesomepicker']);
+        if(isset($this->config)) {
+            $this->config->set('readonly', true);
+            $this->config->set('wrapper', ['class' => 'fontawesomepicker']);
+        } else {
+            $this->settings['readonly'] = true;
+            $this->settings['wrapper'] = ['class' => 'fontawesomepicker'];
+        }
     }
 }
